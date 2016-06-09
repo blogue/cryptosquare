e15f13ae6e2bb190e19a022e3841e07b138fe92d
Benjamin Logue and Kevin VanEvery <kevin@>
2016-06-09 10:13:56 -0700

    add front end controls to index and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 /*This file is for your custom js.  All yours*/
+// front end controls
+$(document).ready(function(){
+
+  $("#input").submit(function(event){
+    event.preventDefault();
+    var input = ($("#raw-message").val());
+
+    var output = cryptosquare(input);
+
+    $('#output').text(output);
+
+  });
+});
+
+//business rules
